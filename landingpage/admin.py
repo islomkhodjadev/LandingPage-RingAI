3c10da81ee5242cd176db1b15dc5d2d8473d5e1b
islomkhodja <islomkhodjadev@gmail.com>
2024-11-07 17:31:02 +0500

    added admin

--- landingpage/admin.py
+++ landingpage/admin.py
@@ -1,3 +1,24 @@
+# admin.py
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "email",
+        "phone",
+        "message",
+        "created_at",
+    )  # Display these fields in the admin list view
+    search_fields = (
+        "name",
+        "email",
+        "phone",
+    )  # Enable search functionality on these fields
+    list_filter = ("created_at",)  # Filter by creation date
+    readonly_fields = ("created_at",)  # Make the created_at field read-only
+
+
+# Register the Contact model with the custom admin class
+admin.site.register(Contact, ContactAdmin)
